0b2244b2ca8793c5d67d7cbc6833ea9619354585
stillgood96 <a787574@naver.com>
2023-08-07 00:31:47 +0900

    storyboard에 outlet, action 생성

--- HelloSwiftProject/ViewController.swift
+++ HelloSwiftProject/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func showValue(_ sender: Any) {
+      let name = inputField.text!
+      valueLabel.text = "Hello, \(name)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
